e0d1a4ed5b12658cfe4d97f693386c69c8edc0d5
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-11-21 15:17:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,45 @@
-//your code here!
+document.addEventListener("DOMContentLoaded", function () {
+    const list = document.getElementById("infi-list");
+    const observerOptions = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.1,
+    };
 
+    // Intersection Observer callback function
+    const intersectionCallback = (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                // User has scrolled to the end of the list
+                // Add 2 more list items
+                addMoreItems();
+            }
+        });
+    };
+
+    // Create Intersection Observer
+    const observer = new IntersectionObserver(
+        intersectionCallback,
+        observerOptions
+    );
+
+    // Start observing the last list item
+    const lastListItem = list.lastElementChild;
+    observer.observe(lastListItem);
+
+    // Function to add more list items
+    const addMoreItems = () => {
+        for (let i = 0; i < 2; i++) {
+            const newItem = document.createElement("li");
+            newItem.textContent = "Item " + (list.childElementCount + 1);
+            list.appendChild(newItem);
+        }
+    };
+
+    // Initial population of the list with 10 items
+    for (let i = 1; i <= 10; i++) {
+        const newItem = document.createElement("li");
+        newItem.textContent = "Item " + i;
+        list.appendChild(newItem);
+    }
+});
